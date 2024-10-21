ef8a73d4d0deda8c5c02dcf7a073a5e4720b5754
Dietmar Gerald Schrausser <dietmar.schrausser@gmail.com>
2024-10-21 04:13:51 +0200

    Create DICE_.c

---
+++ DICE_.c
@@ -0,0 +1,9 @@
+//------------------------------------------------------------------------------| DICE_ by Dietmar Schrausser 2009         //
+#include <dos.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+   system("dice");
+}
