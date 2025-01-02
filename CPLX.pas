3bf1b4bd919ea69584881f88091592a77cd4a0da
Dietmar Gerald Schrausser <dietmar.schrausser@gmail.com>
2025-01-02 15:43:08 +0100

    Create CPLX.pas

---
+++ CPLX.pas
@@ -0,0 +1,30 @@
+#cas
+CPLX(C):=
+BEGIN
+C▶Z1
+RE(Z1)▶R
+IM(Z1)▶I
+ABS(Z1)▶L1(1)
+X▶J
+I▶K
+"Y=√(1-X^2)"▶V5
+"Y=-1*√(1-X^2)"▶V6
+"Y=I"▶V7
+"X=R"▶V8
+"X<R AND X>0 AND Y>0 AND Y≤0.01"▶V0
+"Y=(I/R)*X AND Y>0 AND X<R"▶V9
+IF I<0 THEN
+"Y=(I/R)*X AND Y<0 AND X<R"▶V9
+END;
+IF R<0 THEN
+"Y=(I/R)*X AND Y>0 AND X>R"▶V9
+END;
+IF I<0 AND R<0 THEN
+"Y=(I/R)*X AND Y<0 AND X>R"▶V9
+END;
+ARG(Z1)▶L1(2)
+CONVERT(L1(2)_rad,1_deg)▶L1(3)
+RETURN(L1);
+END;
+#end
+ 
